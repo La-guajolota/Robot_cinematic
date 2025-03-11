676eb0e3a6589c6a5acf2a590b494d8b54611ae7
La-guajolota <adriansilpa@gmail.com>
2025-03-11 10:35:06 -0600

    modified:   app/main.py 	deleted:    app/tests/graph_frames.py 	deleted:    app/utils/direct_dynamic.py 	deleted:    app/utils/obj_joints_links.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,7 @@
 import streamlit as st
-from utils import direct_dynamic as dd
-from utils import obj_joints_links as jl
 import matplotlib.pyplot as plt
 
 #Funcines
-def plot_points(joints_list):
-    for point in joints_list:
-        ax.scatter(point[0], point[1], point[2], color='g')
-
-def plot_vector(joints_list):
-    ax.quiver(0, 0, 0, joints_list[0][0], joints_list[0][1], joints_list[0][2], color='b')
-
-    if len(joints_list) >= 2:
-        for n in range(len(joints_list) - 1):
-            ax.quiver(joints_list[n][0], joints_list[n][1], joints_list[n][2], 
-                      joints_list[n+1][0] - joints_list[n][0], 
-                      joints_list[n+1][1] - joints_list[n][1], 
-                      joints_list[n+1][2] - joints_list[n][2], color='b')
 
 # Configuración de la página
 st.set_page_config(
@@ -37,14 +22,13 @@ with st.container():
     with middle_column:
         st.subheader("Configuración de Eslabones y Juntas")
 
-        num_links = st.number_input("Número de eslabones", min_value=1, max_value=10, value=1)
+        num_links = st.number_input("Número de eslabones", min_value=2, max_value=10, value=1)
         links_list = []
         with st.expander("Configuración de eslabones"):
             for i in range(num_links):
                 st.markdown(f"**Eslabón {i+1}**")
                 length = st.number_input(f"Longitud del eslabón {i+1}", min_value=0.1, max_value=10.0, value=1.0)
                 axis = st.selectbox(f"Eje donde posa el eslabón{i+1}", ['theta', 'phi', 'ro'])
-                links_list.append(jl.Link(length, axis))
                 st.markdown("---")
 
         num_joints = num_links
@@ -53,8 +37,6 @@ with st.container():
             for i in range(num_joints):
                 st.markdown(f"**Junta {i+1}**")
                 angle = st.number_input(f"Ángulo de la junta {i+1}", min_value=0.0, max_value=360.0, value=0.0)
-                #axis = st.selectbox(f"Eje de rotación de la junta {i+1}", ['theta', 'phi', 'ro'])
-                joints_list.append(jl.Joint(angle, axis))
                 st.markdown("---")
 
         # Mostrar datos ingresados
@@ -63,25 +45,3 @@ with st.container():
                 st.write(links_list[i])
             for i in range(num_joints):
                 st.write(joints_list[i])
-
-        #Calculamos el movimiento de los eslabones y juntas
-        point_list = dd.transformation_relative_to_origin_frame(links_list, joints_list)        
-
-        # Plot VECTORS
-        fig = plt.figure(figsize=(10, 6))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim([-5, 5])
-        ax.set_ylim([5, -5])
-        ax.set_zlim([0, 5])
-
-        # Plot points
-        ax.scatter(0, 0, 0, color='r')  # Origin
-
-        # Plot vector
-        plot_points(point_list)
-        plot_vector(point_list)
-
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        st.pyplot(fig, use_container_width=True)
